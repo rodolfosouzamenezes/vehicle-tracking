f38e1986d3c5925817bdc8e34eda673fb429e767
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-03-12 04:26:36 -0300

    refactor(server): move files to root repository

---
+++ server/README.md
@@ -0,0 +1,30 @@
+# TRACKING VEHICLE SERVER
+
+## Instalação
+
+```bash
+npm install
+```
+## Prisma
+
+```bash
+npx prisma generate
+```
+
+
+## Iniciando a aplicação
+
+### Desenvolvimento
+```bash
+npm run start
+```
+
+### Desenvolvimento (Watch Mode)
+```bash
+npm run start:dev
+```
+
+### Produção
+```bash
+npm run start:prod
+```
---
+++ server/src/routes/routes.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { RoutesSerializer } from './routes.serializer';
+
+@Controller('routes')
+export class RoutesController {
+  constructor(private readonly routesService: RoutesService) {}
+
+  @Post()
+  async create(@Body() createRouteDto: CreateRouteDto) {
+    const route = await this.routesService.create(createRouteDto);
+
+    return new RoutesSerializer(route);
+  }
+
+  @Get()
+  async findAll() {
+    const routes = await this.routesService.findAll();
+
+    return routes.map((route) => new RoutesSerializer(route));
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const route = await this.routesService.findOne(id);
+
+    return new RoutesSerializer(route);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
+    return this.routesService.update(+id, updateRouteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.routesService.remove(+id);
+  }
+}
